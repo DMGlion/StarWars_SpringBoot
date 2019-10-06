3a8cc9e393df4ccbc460c4e7f7ccd3cce7089977
David Moya <davidmgarcia6@gmail.com>
2019-10-06 23:06:08 +0100

    Init springBoot prohject

---
+++ src/main/java/com/example/star_wars_springboot/StarWarsSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.star_wars_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarWarsSpringbootApplication.class, args);
+    }
+
+}
